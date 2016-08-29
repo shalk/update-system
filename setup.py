ea27a3d1de7ca22127fb57f3bc7ddcdd10620f3a
shalk <shalk@aliyun.com>
2016-08-29 16:57:42 +0800

    add publish and tag for setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
+import os,sys
 from setuptools import setup,find_packages
+
+version="0.1"
+# from http://www.pydanny.com/python-dot-py-tricks.html
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
 
 setup(
     name = "UpdateSystem",          # 包名
-    version = "0.1",              # 版本信息
+    version = version,              # 版本信息
     packages = find_packages('lib'),  # 要打包的项目文件夹
     package_dir={ '': 'lib' },
     install_requires = [          # 安装依赖的其他包
@@ -13,7 +27,7 @@ setup(
     scripts=[
          'bin/us',
     ],
-    url='www.google.com',
+    #url='www.xshalk.com',
     license='MIT',
     author='shalk',
     author_email='shalk@aliyun.com',
